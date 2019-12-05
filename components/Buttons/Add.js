27ad3b5abe1f698b49e3283b6bab3ae1c92ef827
Gavril <ghmoniaga@gmail.com>
2019-12-05 08:37:57 -0800

    ui tweaks: tab icons, add button, hide search bar

--- components/Buttons/Add.js
+++ components/Buttons/Add.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { View, TouchableOpacity, Text } from 'react-native'
+import { View, TouchableOpacity, Text } from 'react-native-ui-lib'
 import Icon from 'react-native-vector-icons/Fontisto'
 
 import styles from './styles'
 
 export default ({ toggleOverlay }) => (
     <TouchableOpacity style={ [styles.button, styles.addButton] } onPress={ toggleOverlay }>
-        <Icon name="plus-a"/>
+        <View center>
+                <Icon name="plus-a" size={20}/>
+        </View>
     </TouchableOpacity>
 )
